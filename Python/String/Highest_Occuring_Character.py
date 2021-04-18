83c31ebdc135d78968bd8f0102bf9200f7f7be39
Neeraj <nkthecoder@gmail.com>
2021-04-18 22:42:17 +0530

    string

---
+++ Python/String/Highest_Occuring_Character.py
@@ -0,0 +1,13 @@
+str=input()
+var1=sorted(str)
+max=-1
+print(var1)
+for i in range(len(var1)-1):
+    for j in range(i+1,len(var1)):
+        if var1[i]!=var1[j]:
+            s=j-i
+            if max<s:
+                max==s
+                var2=var1[i]         
+            i=j
+print(var2)
