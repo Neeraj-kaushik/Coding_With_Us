6ec5661c18eabd8fbdad389cf263419e4276a89d
Neeraj <nkthecoder@gmail.com>
2021-04-20 20:32:40 +0530

    string

--- Python/String/Highest_Occuring_Character.py
+++ Python/String/Highest_Occuring_Character.py
@@ -5,7 +5,8 @@ print(var1)
 for i in range(len(var1)-1):
     for j in range(i+1,len(var1)):
         if var1[i]!=var1[j]:
-            s=j-i
+            s=j-i 
+            print(s)
             if max<s:
                 max==s
                 var2=var1[i]         
